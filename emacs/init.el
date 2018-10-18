dd0d9a463918049bdda1263579dd06cc958490f0
Rogan Creswick <rogan@groq.com>
2018-10-18 13:26:18 -0700

    notification aliases, ghci redir command

--- emacs/init.el
+++ emacs/init.el
@@ -77,6 +77,19 @@
 ;; More expressive undo
 (global-undo-tree-mode)
 
+;; Handy util to delete things
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      ;; (if (vc-backend filename)
+      ;;     (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))) ;)
+
 ;; ------------------------------------------------------------
 ;; Haskell
 
